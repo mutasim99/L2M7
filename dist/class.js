899eb1fa76bce6fc567eae4a71a621f2b5395f49
mutasim99 <virginmr882@gmail.com>
2025-11-14 11:53:18 +0600

    start to learn oob

---
+++ dist/class.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class Animal {
+    name;
+    species;
+    sound;
+    constructor(name, species, sound) {
+        this.name = name;
+        this.species = species;
+        this.sound = sound;
+    }
+    ;
+}
+;
+const dog = new Animal('dogesh', 'dog', 'gehw ghew');
+console.log(dog.species);
+//# sourceMappingURL=class.js.map
---
+++ src/class.ts
@@ -0,0 +1,15 @@
+class Animal {
+    name: string;
+    species: string;
+    sound: string
+
+    constructor(name: string, species: string, sound: string) {
+        this.name = name;
+        this.species = species;
+        this.sound = sound;
+    };
+};
+
+const dog = new Animal('dogesh', 'dog', 'gehw ghew');
+
+console.log(dog.species);
