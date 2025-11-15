8bee98d0e535935cef7e26a31c53e78359f7fb98
mutasim99 <virginmr882@gmail.com>
2025-11-15 13:18:15 +0600

    add 2nd example of polymorphisnm

--- src/polymorphism.ts
+++ src/polymorphism.ts
@@ -28,6 +28,45 @@ const person1 = new Person();
 const student = new Student();
 const worker = new Worker();
 
-getSleepingHours(person1)
-getSleepingHours(student)
-getSleepingHours(worker)
+// getSleepingHours(person1);
+// getSleepingHours(student);
+// getSleepingHours(worker);
+
+//* Example2
+
+class Shape {
+    getArea(): number {
+        return 0;
+    }
+}
+
+class Circle extends Shape {
+    radius: number;
+
+    constructor(radius: number) {
+        super();
+        this.radius = radius
+    }
+    getArea(): number {
+        return Math.PI * this.radius * this.radius
+    };
+};
+
+class Rectangle extends Shape {
+    height: number;
+    width: number;
+
+    constructor(height: number, width: number) {
+        super();
+        this.height = height;
+        this.width = width
+    }
+    getArea(): number {
+        return this.height * this.width;
+    }
+}
+
+const circleArea = new Circle(6);
+const rectangleArea = new Rectangle(50, 5)
+console.log(circleArea.getArea().toFixed(3));
+console.log(rectangleArea.getArea());
